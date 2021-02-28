5ddcee6c48de50b2f6c4e48a27ec94f7cfa52bbd
Sonlanggtu <DamNgocSonIT@gmail.com>
2021-02-28 22:09:54 +0700

    Ingration Filter  in ASP.NET CORE 3.1

---
+++ Filter/ClaimRequirementFilter.cs
@@ -0,0 +1,51 @@
+﻿using DemoMiddleware.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DemoMiddleware.Filter
+{
+    public class ClaimRequirementFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" >>> OnAuthorization");
+
+            // Claims with No Ingration 3th Identity
+            var user = context.HttpContext.Items["User"];
+            if (user != null)
+            {
+                // code
+            }
+            else
+            {
+                context.Result = new ForbidResult();
+            }
+
+            // Check Claims with Ingration Identity
+            //var claimsIdentity = context.HttpContext.User.Identity as ClaimsIdentity;
+            //var permissionsClaim = context.HttpContext.User.Claims.SingleOrDefault(c => c.Type == SystemConstants.UserClaim.Permissions);
+            //if (permissionsClaim != null)
+            //{
+            //    var permissions = JsonConvert.DeserializeObject<List<string>>(permissionsClaim.Value);
+            //var functionArr = _function.ToString().Split("_");
+            //string functionId = string.Join(".", functionArr);
+            //if (!permissions.Contains(functionId + "_" + _action))
+            //{
+            //    context.Result = new ForbidResult();
+            //}
+            //}
+            //else
+            //{
+            //    context.Result = new ForbidResult();
+            //}
+
+        }
+    }
+}
